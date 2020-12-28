7c956295ced262197e13cbe52f728c36578bd3cd
Jiawei Tang <jit072@ucsd.edu>
2020-12-28 13:15:25 -0800

    added basic function

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+    console.log("bot is up!");
+});
+
+client.on("message", msg => {
+    if (msg.content.startsWith("-") && msg.channel.name !== "bot-cmd") {
+        msg.channel.send("Uncool comrade, uncool. Bot command are for \"bot-cmd\" channel only. Keep this going then you are off to the Gulag.");
+        msg.channel.send("Remember, Big Brother is watching you.");
+    }
+});
+
+client.login("NzkzMjA3NTU2MTgwNTQxNDgw.X-o6Gg.WDgwYDa-yIl71IUwXDHUpv4JNTs");
