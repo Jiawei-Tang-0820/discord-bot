2b4adbf441cf232d0da2bb12e80be8bc14357381
Jiawei Tang <jit072@ucsd.edu>
2020-12-28 15:58:40 -0800

    read token from secret

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+const fs = require("fs")
+const Discord = require("discord.js")
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 client.once("ready", () => {
-    console.log("bot is up!");
+    console.log("KGB is up!")
 });
 
 client.on("message", msg => {
     if (msg.content.startsWith("-") && msg.channel.name !== "bot-cmd") {
-        msg.channel.send("Uncool comrade, uncool.");
-        msg.channel.send("Bot commands are for \"bot-cmd\" channel only. Keep this going and you are off to the Gulag.");
-        msg.channel.send("Remember, Big Brother is watching you.");
+        msg.channel.send("Uncool comrade, uncool.")
+        msg.channel.send("Bot commands are for \"bot-cmd\" channel only. Keep this going and you are off to the Gulag.")
+        msg.channel.send("Remember, Big Brother is watching you.")
     }
 });
 
-client.login("NzkzMjA3NTU2MTgwNTQxNDgw.X-o6Gg.WDgwYDa-yIl71IUwXDHUpv4JNTs");
+fs.readFile("./secret", (err, data) => {
+    if (err) {
+        console.log("failed to read the secret file!")
+        console.log(err)
+        return
+    }
+    const token = data.toString()
+    client.login(token)
+})
