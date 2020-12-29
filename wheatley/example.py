53920b9a52655e7070306d2b7b4652849b67ffbc
Cecilia Xiao <yux164@ucsd.edu>
2020-12-28 18:36:26 -0800

    add eyes react

--- wheatley/example.py
+++ wheatley/example.py
@@ -1,4 +1,6 @@
 import discord
+import asyncio
+
 
 client = discord.Client()
 with open("./secret") as f:
@@ -7,7 +9,6 @@ with open("./secret") as f:
 @client.event
 async def on_ready():
     print("Helloooo?")
-    print("Are you going to open the door? At any time?")
 
 @client.event
 async def on_message(message):
@@ -16,5 +17,8 @@ async def on_message(message):
 
     if "moron" in message.content.lower():
         await message.channel.send("I am NOT! A MORON!")
+
+    if "Wheatley" in [usr.name for usr in message.mentions]:
+        await message.add_reaction("👀")
 
 client.run(token)
